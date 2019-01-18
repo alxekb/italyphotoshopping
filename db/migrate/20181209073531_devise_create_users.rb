8196adacd1755b90aa7c11d20b3dfadef21d2c90
AlexIv <alx.ekb@gmail.com>
2019-01-18 23:30:49 +0100

    migration

--- db/migrate/20181209073531_devise_create_users.rb
+++ db/migrate/20181209073531_devise_create_users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
-  def up
+  def change
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -36,12 +36,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
-    def down
-      drop_table :users
-    end
-
-    # add_index :users, :email,                unique: true
-    # add_index :users, :reset_password_token, unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
