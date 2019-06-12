405b464b3f60541fc1c4c698549a587cc937d9c7
AlexIv <alx.ekb@gmail.com>
2019-06-12 20:12:33 +0200

    WARNING. Migrations item.model_id manually added column

--- db/migrate/20190610200106_add_brand_id_to_items.rb
+++ db/migrate/20190610200106_add_brand_id_to_items.rb
@@ -1,5 +1,7 @@
 class AddBrandIdToItems < ActiveRecord::Migration[5.2]
   def change
     add_reference :items, :brand, foreign_key: true
+    # this string won't go down. Comment it out before rolling down.
+    add_reference :items, :model, foreign_key: true
   end
 end
