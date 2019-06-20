bd142531d1435e6ee7ab9887b3505a3cf2df30ee
AlexIv <alx.ekb@gmail.com>
2019-06-20 14:26:27 +0200

    hh

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   def full_name_with_id
-    "#" + self.id + " " self.name + " " + self.second_name + " " + self.surname
+    self.id + " " self.name + " " + self.second_name + " " + self.surname
   end
 
 
