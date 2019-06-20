73024f640ee634b21653ae56c6c2548ed631a622
AlexIv <alx.ekb@gmail.com>
2019-06-20 14:22:50 +0200

    UI improvement

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,11 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
+  def full_name_with_id
+    "#" + self.id + " " self.name + " " + self.second_name + " " + self.surname
+  end
+
+
   def full_name
     self.name + " " + self.second_name + " " + self.surname
   end
